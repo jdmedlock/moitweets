5ed32fbf29b4757468a8411b0cc277fb48792353
jdmedlock <jdmedlock@gmail.com>
2020-01-18 13:46:51 -0600

    Feature: Setup backend server with routes
    Setup backend server with `gettweets` route. Note that error checking remains to be added.
    
    Resolves: N/a
    See also: N/a

--- server/src/getTweets.js
+++ server/src/getTweets.js
@@ -1,41 +1,48 @@
-const request = require("request")
-
+const request = require("request-promise-native")
+/**
+ * Retrieve a bearer token for the Twitter API. This depends on the
+ * environment variables `TWITTER_API_KEY` and `TWITTER_API_SECRET` being 
+ * defined with the MoiTweets application key and secret 
+ * (see https://developer.twitter.com)
+ * @returns {String} Twitter API bearer token
+ */
 const getBearerToken = async () => {
-    const key = process.env.TWITTER_API_KEY
-    const secret = process.env.TWITTER_API_SECRET
-    const cat = key + ":" + secret
-    const credentials = new Buffer(cat).toString('base64')
+  const apiKey = process.env.TWITTER_API_KEY
+  const apiSecret = process.env.TWITTER_API_SECRET
+  const key = apiKey + ":" + apiSecret
+  const credentials = new Buffer(key).toString('base64')
 
-    const url = 'https://api.twitter.com/oauth2/token'
+  const url = 'https://api.twitter.com/oauth2/token'
 
-    request({ 
-        url: url,
-        method:'POST',
-        headers: {
-            "Authorization": "Basic " + credentials,
-            "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
-        },
-        body: "grant_type=client_credentials"
-    }, function(err, resp, body) {
-        console.dir(body); //the bearer token...
-    });
+  const bearerToken = await request({ 
+    url: url,
+    method:'POST',
+    headers: {
+        "Authorization": "Basic " + credentials,
+        "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
+    },
+    body: "grant_type=client_credentials",
+    json: true
+  })
+  return  bearerToken.access_token;
 }
 
 const getTweets = async (twitterName) => {
-    const bearerToken = await getBearerToken()
-    const url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+  let bearerToken = await getBearerToken()
+  const url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 
-    request({ 
-        url: url,
-        method:'GET',
-        qs:{"screen_name":"chingucollabs"},
-        json:true,
-        headers: {
-            "Authorization": "Bearer " + bearerToken
-        }
-    }, function(err, resp, body) {
-        console.dir(body)
-    });
+  const tweetsJSON = await request({ 
+    url: url,
+    method: 'GET',
+    qs:{"screen_name": twitterName},
+    json:true,
+    headers: {
+        "Authorization": "Bearer " + bearerToken
+    },
+    json: true
+  })
+  bearerToken = null
+  return tweetsJSON;
 }
 
 module.exports = getTweets
--- server/src/server.js
+++ server/src/server.js
@@ -1,37 +1,38 @@
-'use strict';
-const dotenv = require('dotenv');
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
+'use strict'
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const express = require('express')
+const path = require('path')
+const serverless = require('serverless-http')
 const getTweets = require('./getTweets')
 
-const app = express();
+const app = express()
 const router = express.Router();
 
-
-const result = dotenv.config();
+const result = dotenv.config()
 if (result.error) {
   throw result.error
 }
 
-getTweets('jd_medlock')
-
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>MoiTweets Backend Server is up and running!</h1>');
-  res.end();
-});
-
-// Process a request to retrieve Tweets
-router.get('/gettweets', (req, res) => {
-  getTweets(req.twitterName)
-  res.json({ data: req.twitterName }) // Just for testing
-});
-
+app.use(express.static(path.join(__dirname, 'client')))
 app.use(bodyParser.json())
-app.use('/.netlify/functions/server', router)  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+// Process a request to the default route
+app.get('/', (req, res) => {
+  res.send('MoiTweets backend is active')
+})
+
+// Process a request to retrieve Tweets for a specific user
+app.get('/gettweets/:id', async (req, res) => {
+  const tweetsJSON = await getTweets(req.params.id)
+  res.send({ data: tweetsJSON }) // Just for testing
+})
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app
 module.exports.handler = serverless(app)
